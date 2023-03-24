da5caba55fd876fd527d46a32e1c641d9920e23c
AurelienLouvel <79204651+AurelienLouvel@users.noreply.github.com>
2023-03-24 17:55:41 +0100

    Build

--- src/url.js
+++ src/url.js
@@ -2,7 +2,7 @@ const restoreUrl = () => {
     return {
         name: "inject-script", transformIndexHtml(html) {
             return html.replace("</body>",
-                `<script>document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll("[href]").forEach((function(t){let e=t.getAttribute("href");e&&e.startsWith("/")&&t.setAttribute("href",window.location.href+e)}))}));</script></body>`)
+                `<script>document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll("[href], [src]").forEach((function(t){let e=t.getAttribute("href");if(e&&e.startsWith("/")){let r=window.location.href+e;t.hasAttribute("href")?t.href=r:t.setAttribute("href",r)}let r=t.getAttribute("src");if(r&&r.startsWith("/")){let e=window.location.href+r;t.hasAttribute("src")?t.src=e:t.setAttribute("src",e)}}))}));</script></body>`)
         }
     }
 }
