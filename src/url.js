d6cb2bc9fa72fd9c0547f394c23bef4b2494511f
AurelienLouvel <79204651+AurelienLouvel@users.noreply.github.com>
2023-03-24 17:49:03 +0100

    Build

--- src/url.js
+++ src/url.js
@@ -2,7 +2,7 @@ const restoreUrl = () => {
     return {
         name: "inject-script", transformIndexHtml(html) {
             return html.replace("</body>",
-                `<script>document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll("[href],[src]").forEach((function(t){let e=t.getAttribute("href")||t.getAttribute("src");e&&e.startsWith("/")&&t.setAttribute("href",window.location.href+e)}))}));</script></body>`)
+                `<script>document.addEventListener("DOMContentLoaded",(function(){document.querySelectorAll("[href]").forEach((function(t){let e=t.getAttribute("href");e&&e.startsWith("/")&&t.setAttribute("href",window.location.href+e)}))}));</script></body>`)
         }
     }
 }
