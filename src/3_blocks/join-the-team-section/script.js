e10911e4c54484acd2b366ec413d39983b22da8b
Roulianosss <julien.ortscheid@gmail.com>
2023-04-13 17:41:15 +0200

    new section added

--- src/3_blocks/join-the-team-section/script.js
+++ src/3_blocks/join-the-team-section/script.js
@@ -4,13 +4,16 @@ allOfferBtns.forEach(btn => btn.addEventListener('click', handleClick))
 
 
 function handleClick() {
-    console.log(this.nextElementSibling.style.maxHeight)
+    console.dir(this)
     if (this.nextElementSibling.style.maxHeight) {
         
         console.dir(this.nextElementSibling)
         this.nextElementSibling.style.maxHeight = ''
+        this.lastElementChild.style.transform = 'rotate(0deg)'
     } else {
         const contentHeight = this.nextElementSibling.scrollHeight
         this.nextElementSibling.style.maxHeight = `${contentHeight}px`
+        this.lastElementChild.style.transform = 'rotate(180deg)'
+        
     }
 }
