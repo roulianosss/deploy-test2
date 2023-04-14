1ccfa28c28e9cd2323995ca4bf5d1e141eefa34d
(oré) <79204651+aurelienlouvel@users.noreply.github.com>
2023-04-14 17:34:49 +0200

    fix(articleCard): breakpoint

--- src/ads.js
+++ src/ads.js
@@ -6,39 +6,6 @@ import tab from "./1_components/tab/template.html?raw"
 import link from "./1_components/link/template.html?raw"
 import fileLink from "./1_components/file-link/template.html?raw"
 import tag from "./1_components/tag/template.html?raw"
-
-const components = [
-    {
-        name: "button",
-        template: button
-    },
-    {
-        name: "iconButton",
-        template: iconButton
-    },
-    {
-        name: "toggleButton",
-        template: toggleButton
-    },
-    {
-        name: "tab",
-        template: tab
-    },
-    {
-        name: "link",
-        template: link
-    },
-    {
-        name: "fileLink",
-        template: fileLink
-    },
-    {
-        name: "tag",
-        template: tag
-    }
-
-]
-
 // PATTERNS
 import breadcrumb from "./2_patterns/breadcrumb/template.html?raw"
 import tabsNavigation from "./2_patterns/tabs-navigation/template.html?raw"
@@ -48,43 +15,6 @@ import awardItem from "./2_patterns/award-item/template.html?raw"
 import feeCard from "./2_patterns/fee-card/template.html?raw"
 import managementGuidedCard from "./2_patterns/management-guided-card/template.html?raw"
 import managementOthersCard from "./2_patterns/management-others-card/template.html?raw"
-
-
-const patterns = [
-    {
-        name: "breadcrumb",
-        template: breadcrumb
-    },
-    {
-        name: "tabsNavigation",
-        template: tabsNavigation
-    },
-    {
-        name: "keypointCard",
-        template: keypointCard
-    },
-    {
-        name: "articleCard",
-        template: articleCard
-    },
-    {
-        name: "awardItem",
-        template: awardItem
-    },
-    {
-        name: "feeCard",
-        template: feeCard
-    },
-    {
-        name: "managementGuidedCard",
-        template: managementGuidedCard
-    },
-    {
-        name: "managementOthersCard",
-        template: managementOthersCard
-    }
-]
-
 // BLOCKS
 import header from "./3_blocks/header/template.html?raw"
 import footer from "./3_blocks/footer/template.html?raw"
@@ -129,195 +59,143 @@ import reasonToJoinSection from "./3_blocks/reason-to-join-section/template.html
 import discoverOurJobSection from "./3_blocks/discover-our-job-section/template.html?raw"
 import joinTheTeamSection from "./3_blocks/join-the-team-section/template.html?raw"
 
-const blocks = [
-    {
-        name: "header",
-        template: header
-    },
-    {
-        name: "footer",
-        template: footer
-    },
-    {
-        name: "wrapper (66/33 - 50/50 - 33/66)",
-        template: wrapper
-    },
-    {
-        name: "homeHeroBanner",
-        template: homeHeroBanner
-    },
-    {
-        name: "promoBanner",
-        template: promoBanner
-    },
-    {
-        name: "companyValueSection",
-        template: companyValueSection
-    },
-    {
-        name: "advicesSection",
-        template: advicesSection
-    },
-    {
-        name: "insurancePartnersSection",
-        template: insurancePartnersSection
-    },
-    {
-        name: "investmentDisclaimerSection",
-        template: investmentDisclaimerSection
-    },
-    {
-        name: "awardsSection",
-        template: awardsSection
-    },
-    {
-        name: "reviewsSection",
-        template: reviewsSection
-    },
-    {
-        name: "partnerNewsSection",
-        template: partnerNewsSection
-    },
-    {
-        name: "productHeroBanner",
-        template: productHeroBanner
-    },
-    {
-        name: "productAdvantagesSection",
-        template: productAdvantagesSection
-    },
-    {
-        name: "productManagementSection",
-        template: productManagementSection
-    },
-    {
-        name: "productInsurancePartnerSection",
-        template: productInsurancePartnerSection
-    },
-    {
-        name: "productContractualDocumentationSection",
-        template: productContractualDocumentationSection
-    },
-    {
-        name: "productFeesSection",
-        template: productFeesSection
-    },
-    {
-        name: "faqRedirectionSection",
-        template: faqRedirectionSection
-    },
-    {
-        name: "managementAdvantagesSection",
-        template: managementAdvantagesSection
-    },
-    {
-        name: "managementPlusSection",
-        template: managementPlusSection
-    },
-    {
-        name: "assetsSupportSection",
-        template: assetsSupportSection
-    },
-    {
-        name: "assetsPilotedSection",
-        template: assetsPilotedSection
-    },
-    {
-        name: "partnerPilotedSection",
-        template: partnerPilotedSection
-    },
-    {
-        name: "headerPilotedSection",
-        template: headerPilotedSection
-    },
-    {
-        name: "headerSupportSection",
-        template: headerSupportSection
-    },
-    {
-        name: "contractsPilotedSection",
-        template: contractsPilotedSection
-    },
-    {
-        name: "contractsSupportSection",
-        template: contractsSupportSection
-    },
-    {
-        name: "altaNewsTalkingAboutSection",
-        template: altaNewsTalkingAboutSection
-    },
-    {
-        name: "webconferencesSection",
-        template: webconferencesSection
-    },
-    {
-        name: "pressSection",
-        template: pressSection
-    },
-    {
-        name: "expertsVoiceSection",
-        template: expertsVoiceSection
-    },
-    {
-        name: "headerExpertsVoiceSection",
-        template: headerExpertsVoiceSection
-    },
-    {
-        name: "discoverIntegrationProgramSection",
-        template: discoverIntegrationProgramSection
-    },
-    {
-        name: "questionsSection",
-        template: questionsSection
-    },
-    {
-        name: "squadSection",
-        template: squadSection
-    },
-    {
-        name: "altaInNumberSection",
-        template: altaInNumberSection
-    },
-    {
-        name: "altaprofitHistorySection",
-        template: altaprofitHistorySection
-    },
-    {
-        name: "reasonToJoinSection",
-        template: reasonToJoinSection
-    },
-    {
-        name: "discoverOurJobSection",
-        template: discoverOurJobSection
-    },
-    {
-        name: "joinTheTeamSection",
-        template: joinTheTeamSection
-    }
+const components = [{
+    name: "button", template: button
+}, {
+    name: "iconButton", template: iconButton
+}, {
+    name: "toggleButton", template: toggleButton
+}, {
+    name: "tab", template: tab
+}, {
+    name: "link", template: link
+}, {
+    name: "fileLink", template: fileLink
+}, {
+    name: "tag", template: tag
+}
+
 ]
+
+
+const patterns = [{
+    name: "breadcrumb", template: breadcrumb
+}, {
+    name: "tabsNavigation", template: tabsNavigation
+}, {
+    name: "keypointCard", template: keypointCard
+}, {
+    name: "articleCard", template: articleCard
+}, {
+    name: "awardItem", template: awardItem
+}, {
+    name: "feeCard", template: feeCard
+}, {
+    name: "managementGuidedCard", template: managementGuidedCard
+}, {
+    name: "managementOthersCard", template: managementOthersCard
+}]
+
+const blocks = [{
+    name: "header", template: header
+}, {
+    name: "footer", template: footer
+}, {
+    name: "wrapper (66/33 - 50/50 - 33/66)", template: wrapper
+}, {
+    name: "homeHeroBanner", template: homeHeroBanner
+}, {
+    name: "promoBanner", template: promoBanner
+}, {
+    name: "companyValueSection", template: companyValueSection
+}, {
+    name: "advicesSection", template: advicesSection
+}, {
+    name: "insurancePartnersSection", template: insurancePartnersSection
+}, {
+    name: "investmentDisclaimerSection", template: investmentDisclaimerSection
+}, {
+    name: "awardsSection", template: awardsSection
+}, {
+    name: "reviewsSection", template: reviewsSection
+}, {
+    name: "partnerNewsSection", template: partnerNewsSection
+}, {
+    name: "productHeroBanner", template: productHeroBanner
+}, {
+    name: "productAdvantagesSection", template: productAdvantagesSection
+}, {
+    name: "productManagementSection", template: productManagementSection
+}, {
+    name: "productInsurancePartnerSection", template: productInsurancePartnerSection
+}, {
+    name: "productContractualDocumentationSection", template: productContractualDocumentationSection
+}, {
+    name: "productFeesSection", template: productFeesSection
+}, {
+    name: "faqRedirectionSection", template: faqRedirectionSection
+}, {
+    name: "managementAdvantagesSection", template: managementAdvantagesSection
+}, {
+    name: "managementPlusSection", template: managementPlusSection
+}, {
+    name: "assetsSupportSection", template: assetsSupportSection
+}, {
+    name: "assetsPilotedSection", template: assetsPilotedSection
+}, {
+    name: "partnerPilotedSection", template: partnerPilotedSection
+}, {
+    name: "headerPilotedSection", template: headerPilotedSection
+}, {
+    name: "headerSupportSection", template: headerSupportSection
+}, {
+    name: "contractsPilotedSection", template: contractsPilotedSection
+}, {
+    name: "contractsSupportSection", template: contractsSupportSection
+}, {
+    name: "altaNewsTalkingAboutSection", template: altaNewsTalkingAboutSection
+}, {
+    name: "webconferencesSection", template: webconferencesSection
+}, {
+    name: "pressSection", template: pressSection
+}, {
+    name: "expertsVoiceSection", template: expertsVoiceSection
+}, {
+    name: "headerExpertsVoiceSection", template: headerExpertsVoiceSection
+}, {
+    name: "discoverIntegrationProgramSection", template: discoverIntegrationProgramSection
+}, {
+    name: "questionsSection", template: questionsSection
+}, {
+    name: "squadSection", template: squadSection
+}, {
+    name: "altaInNumberSection", template: altaInNumberSection
+}, {
+    name: "altaprofitHistorySection", template: altaprofitHistorySection
+}, {
+    name: "reasonToJoinSection", template: reasonToJoinSection
+}, {
+    name: "discoverOurJobSection", template: discoverOurJobSection
+}, {
+    name: "joinTheTeamSection", template: joinTheTeamSection
+}]
 
 
 // RENDER
 
-const categories = [
-    {
-        name: "components",
-        value: components
-    },
-    {
-        name: "patterns",
-        value: patterns
-    },
-    {
-        name: "blocks",
-        value: blocks
-    }
-]
+const categories = [{
+    name: "components", value: components
+}, {
+    name: "patterns", value: patterns
+}, {
+    name: "blocks", value: blocks
+}]
 
 categories.forEach((category) => {
 
     const categorySection = document.createElement("section")
-    categorySection.classList.add("ads")
+    categorySection.classList.add("ads",
+        category.name)
 
     const categoryTitle = document.createElement("h3")
     categoryTitle.innerText = category.name.toUpperCase()
