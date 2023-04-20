5a942eae59dabf93ba014505980940534ea7314d
(oré) <79204651+aurelienlouvel@users.noreply.github.com>
2023-04-20 17:22:13 +0200

    feat(header): place expand when overflow

--- src/3_blocks/header/script.js
+++ src/3_blocks/header/script.js
@@ -1,24 +1,59 @@
-const menuItems = document.querySelectorAll(".menu__item")
+if (document.querySelector('.header')) {
 
-if (menuItems) {
+  const menuItems = document.querySelectorAll('.menu__item');
+  const headerMenu = document.querySelector('.header__menu');
+
+  function init() {
+    placeMenuItems(menuItems);
+    displayExpandMenuItems(menuItems);
+  }
+
+  window.addEventListener('resize', () => {
+    placeMenuItems(menuItems);
+  });
+
+  init();
+
+  function displayExpandMenuItems(menuItems) {
     menuItems.forEach((menuItem) => {
-        menuItem.addEventListener("click",
-            (e) => {
-                e.preventDefault()
-                removeCurrentExpandMenu()
-                toggleExpandMenu(menuItem)
-            })
-    })
-}
+      menuItem.addEventListener('click', (e) => {
+        e.preventDefault();
+        removeCurrentItemClass();
+        addItemClass(menuItem);
+      });
+    });
+  }
 
-function removeCurrentExpandMenu() {
-    const expandMenu = document.querySelectorAll(".expand-menu")
-    expandMenu.forEach((item) => {
-        item.classList.remove("active")
-    })
-}
+  function removeCurrentItemClass() {
+    menuItems.forEach((menuItem) => {
+      menuItem.classList.remove('active');
+    });
+  }
 
-function toggleExpandMenu(menuItem) {
-    const expandMenu = menuItem.querySelector(".expand-menu")
-    expandMenu.classList.toggle("active")
+  function addItemClass(menuItem) {
+    menuItem.classList.add('active');
+  }
+
+  function getElementRightPosition(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.right;
+  }
+
+  function placeMenuItems(menuItems) {
+
+    menuItems.forEach((menuItem) => {
+      const expandMenuItem = menuItem.querySelector('.expand-menu');
+
+      if (getElementRightPosition(expandMenuItem) >
+          getElementRightPosition(headerMenu)) {
+        menuItem.classList.add('menu__item--right');
+        menuItem.classList.remove('menu__item--left');
+      } else {
+        menuItem.classList.remove('menu__item--right');
+        if (menuItem.getAttribute('data-position') === 'left') {
+          menuItem.classList.add('menu__item--left');
+        }
+      }
+    });
+  }
 }
