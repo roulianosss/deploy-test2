291d0421e50124c1197930f4a00f9324905f40e9
aurelienlouvel <79204651+aurelienlouvel@users.noreply.github.com>
2023-05-16 14:12:48 +0200

    💄(logos): contain img

--- src/2_patterns/management-others-card/script.js
+++ src/2_patterns/management-others-card/script.js
@@ -1,43 +1,40 @@
-const managementOthersCards = document.querySelectorAll(".management-others-card")
+const managementOthersCards = document.querySelectorAll('.management-others-card');
 
 if (managementOthersCards) {
-    let maxHeight = 0
-    managementOthersCards.forEach((element) => {
-        const explanations = element.querySelectorAll(".management-others-card__explaination")
+  let maxHeight = 0;
+  managementOthersCards.forEach((element) => {
+    const explanations = element.querySelectorAll('.management-others-card__explaination');
 
-        for (let i = 0;
-             i < explanations.length;
-             i++) {
-            const explanation = explanations[i]
-            const explanationHeight = explanation.offsetHeight
-            if (explanationHeight > maxHeight) {
-                maxHeight = explanationHeight
-            }
-        }
+    for (let i = 0;
+        i < explanations.length;
+        i++) {
+      const explanation = explanations[i];
+      const explanationHeight = explanation.offsetHeight;
+      if (explanationHeight > maxHeight) {
+        maxHeight = explanationHeight;
+      }
+    }
 
-        element.querySelector(".management-others-card__explainations")
-            .style
-            .setProperty(
-                "height",
-                `calc(${maxHeight}px + var(--spacing-8))`
-            )
+    element.querySelector('.management-others-card__explainations').style.setProperty(
+        'height',
+        `calc(${maxHeight}px + var(--spacing-8))`,
+    );
 
-        const links = element.querySelectorAll(".management-others-card__link")
+    const links = element.querySelectorAll('.management-others-card__link');
 
-        links.forEach((link) => {
-            let name = link.dataset.name
-            let explaination = element.querySelector(".management-others-card__explaination[data-name=" + name + "]")
+    links.forEach((link) => {
+      let name = link.dataset.name;
+      let explaination = element.querySelector('.management-others-card__explaination[data-name=' + name + ']');
 
-            link.addEventListener("mouseenter",
-                () => {
-                    explaination.classList.add("active")
-                })
+      link.addEventListener('mouseenter',
+          () => {
+            explaination.classList.add('active');
+          });
 
-            link.addEventListener("mouseleave",
-                () => {
-                    explaination.classList.remove("active")
-                })
-        })
-
-    })
+      link.addEventListener('mouseleave',
+          () => {
+            explaination.classList.remove('active');
+          });
+    });
+  });
 }
